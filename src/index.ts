2cbb9f564007c89df3d88d486d6372d5b4ef9576
hikaru <827394320@qq.com>
2024-10-31 19:26:36 +0800

    🐳 chore: 优化上传逻辑，区分库的upload功能和自己的my_upload功能

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,11 @@
 import { join, resolve } from 'node:path'
+import { my_upload } from './my_upload'
 import { upload } from './upload'
 
 const main = async () => {
     const localPath = join(resolve(), './example/vue-app-example/dist')
     const remotePath = '/home/wyj/send_test'
-    upload({ localPath, remotePath })
+    my_upload({ localPath, remotePath })
 }
 
 main()
